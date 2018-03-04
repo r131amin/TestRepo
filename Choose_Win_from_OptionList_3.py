5b172cee072e29d99948482c036e34f776057351
Ahmed <Ahmed@gmail.com>
2018-03-04 11:44:08 +0200

    new commit 2

--- Choose_Win_from_OptionList_3.py
+++ Choose_Win_from_OptionList_3.py
@@ -4,24 +4,7 @@ from Emp_Window import *
 class Open_Window:
     def display_Form(self,choosenform):
         root = Tk()
-        root.title(choosenform+"  Second Window ")
-        labelframe = LabelFrame(root, text=choosenform+" Data ")
-        labelframe.pack(fill="both", expand="yes")
-
-        if choosenform=='Emp':
-            emp=Emp_Window(root, labelframe)
-        
-
-
-##o=Open_Window()
-##o.display_Form("EMP")
-from tkinter import *
-from Emp_Window import *
-
-class Open_Window:
-    def display_Form(self,choosenform):
-        root = Tk()
-        root.title(choosenform+"  Second Window ")
+        root.title(choosenform+" Window ")
         labelframe = LabelFrame(root, text=choosenform+" Data ")
         labelframe.pack(fill="both", expand="yes")
 
